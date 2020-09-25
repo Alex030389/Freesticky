8197defc6e9041d1190e3532e9f4cc4c2114cbab
AlexKa <x182838@gmail.com>
2020-09-25 16:20:21 +0300

    btn like and load more

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ var gulp        = require('gulp'),
   cheerio       = require('gulp-cheerio'),
   replace       = require('gulp-replace');
 
+  // //////////////////////////////////////////////// localhost
 gulp.task('browser-sync', function() {
   browserSync({
     server: {
@@ -28,59 +29,67 @@ gulp.task('browser-sync', function() {
   })
 });
 
+// ///////////////////////////////////////////////////// clean
 gulp.task('clean', function () {
   return del(['app/assets/template/css', 'app/assets/template/js']);
 });
 
+// ////////////////////////////////////////////////////// html
 gulp.task('html', function() {
   return gulp.src('app/*.html')
   .pipe(browserSync.stream())
 });
 
+// ///////////////////////////////////////////////////// styles
 gulp.task('styles', function() {
   return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
-  // .pipe(sourcemaps.init())
+  .pipe(sourcemaps.init())
   .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
   .pipe(rename({ suffix: '.min', prefix : '' }))
   .pipe(autoprefixer(['last 15 versions']))
-  .pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Opt., comment out when debugging
-  // .pipe(sourcemaps.write())
+  .pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('app/assets/template/css'))
   .pipe(gulp.dest('./'))
   .pipe(browserSync.stream())
 });
 
+gulp.task('styles:build', function() {
+  return gulp.src('app/'+syntax+'/**/*.'+syntax+'')
+  .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+  .pipe(rename({ suffix: '.min', prefix : '' }))
+  .pipe(autoprefixer(['last 15 versions']))
+  .pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
+  .pipe(gulp.dest('app/assets/template/css'))
+  .pipe(gulp.dest('./'))
+});
+
+// ///////////////////////////////////////////////////////// scripts
 gulp.task('scripts', function() {
   return gulp.src([
     'app/libs/svg4everybody/svg4everybody.min.js',
     'app/libs/selectric/jquery.selectric.min.js',
     'app/_js/common.js'
     ])
-  // .pipe(sourcemaps.init())
+  .pipe(sourcemaps.init())
   .pipe(concat('scripts.min.js'))
-  // .pipe(babel({
-  //   presets: ['@babel/env']
-  // }))
-  .pipe(uglify())
-  // .pipe(sourcemaps.write())
+  .pipe(sourcemaps.write())
   .pipe(gulp.dest('app/assets/template/js'))
-  .pipe(browserSync.reload({ stream: true }))
+  .pipe(browserSync.stream())
 });
 
-gulp.task('watch', function() {
-  gulp.watch('app/*.html', gulp.parallel('html'));
-  gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
-  gulp.watch('app/_js/**/*', gulp.parallel('scripts'));
+gulp.task('scripts:build', function() {
+  return gulp.src([
+    'app/libs/svg4everybody/svg4everybody.min.js',
+    'app/libs/selectric/jquery.selectric.min.js',
+    'app/_js/common.js'
+    ])
+  .pipe(concat('scripts.min.js'))
+  .pipe(uglify())
+  .pipe(gulp.dest('app/assets/template/js'))
 });
 
-gulp.task('default', gulp.parallel(
-  'clean',
-  'styles',
-  'scripts',
-  'browser-sync',
-  'watch'
-));
-
+// /////////////////////////////////////////////////////// svg
 gulp.task('svg', function () {
   return gulp.src('app/_img/icons/svg/*.svg')
   // minify svg
@@ -111,7 +120,7 @@ gulp.task('svg', function () {
     .pipe(gulp.dest('app/assets/template/img/icons/svg/'));
 });
 
-// for color icon
+// //////////////////////////////////////////////////////////////// svg-color
 gulp.task('svg-color', function () {
   return gulp.src('app/_img/icons/svg/color/*.svg')
   // minify svg
@@ -141,3 +150,25 @@ gulp.task('svg-color', function () {
     }))
     .pipe(gulp.dest('app/assets/template/img/icons/svg/color/'));
 });
+
+// ////////////////////////////////////////////////////////////// watch
+gulp.task('watch', function() {
+  gulp.watch('app/*.html', gulp.parallel('html'));
+  gulp.watch('app/'+syntax+'/**/*.'+syntax+'', gulp.parallel('styles'));
+  gulp.watch('app/_js/**/*', gulp.parallel('scripts'));
+});
+
+// ///////////////////////////////////////////////////////////// default
+gulp.task('default', gulp.parallel(
+  'clean',
+  'styles',
+  'scripts',
+  'browser-sync',
+  'watch'
+));
+
+gulp.task('build', gulp.series(
+  'clean',
+  'styles:build',
+  'scripts:build',
+));
